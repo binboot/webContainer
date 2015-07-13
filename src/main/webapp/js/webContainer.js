ce5685d66a300ccfae5603a270fa171d8778c9d6
binboot <binboot@hotmail.com>
2015-07-14 07:32:51 +0800

    修改webContainer

--- src/main/webapp/js/webContainer.js
+++ src/main/webapp/js/webContainer.js
@@ -1,13 +1,10 @@
 var globalVar={
-		pin:"binboot",
-		name:"binboot",
-		phone:"18506414123",
-		news:null
+		pin:"",
+		phoneNumber:""
 }
 $(document).ready(
 		function()
 		{
-			var sendDate=$.datepicker.parseDate( "yy-mm-dd", "20070126" );
-			alert(sendDate.getTime());
+			
 		}
 );
